e91b94aeb794b13d0776e0d4b3e937fe9422f5fd
fcox <isc-fco@outlook.com>
2019-01-16 18:41:05 -0600

    RestServer - usuario

---
+++ server/server.js
@@ -0,0 +1,27 @@
+require('./config/config');
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(require('./routes/usuario'));
+
+
+mongoose.connect(process.env.URLDB, { 
+    useNewUrlParser: true,
+    useCreateIndex: true
+    }, (err,res) => {
+
+    if (err) throw err;
+
+    console.log('Bases de datos online');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando el puerto: ', process.env.PORT);
+});
